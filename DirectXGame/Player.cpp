2065c9ad33c7e11b9ffddd4b7ed0bc3a7d43abfc
FuruyaYuto <k024g0025@g.neec.ac.jp>
2025-05-19 16:20:44 +0900

    2-3

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -1,42 +1,16 @@
 #include "Player.h"
-#include <KamataEngine.h>
-#include "MyMath.h"
+#include <cassert>
+using namespace KamataEngine;
 
-void Player::Initialize(KamataEngine::Model* model, uint32_t textureHandle, KamataEngine::Camera* camera) {
+void Player::Initialize(Model* model, uint32_t textureHandle, Camera* camera) {
+	assert(model);
 	model_ = model;
 	textureHandle_ = textureHandle;
+	worldTransform_.Initialize();
 	camera_ = camera;
-
-	assert(model_);
-
-	 worldTransform_.scale_ = KamataEngine::Vector3(10.0f, 10.0f, 10.0f);
-	worldTransform_.translation_ = KamataEngine::Vector3(0.0f, 0.0f, 0.0f); // カメラに対して適切な位置
-	worldTransform_.Initialize();
-
-	// 必ず初期化！
-	worldTransform_.Initialize();
 }
-
-void Player::Update() {
-	// キャラの位置調整（ブロックと重ならない位置に変更）
-	
-	worldTransform_.translation_.x = 5.0f; // X座標を適切に変更
-	worldTransform_.translation_.y = 1.0f; // Y座標を適切に変更
-	worldTransform_.translation_.z = 5.0f; // Z座標を適切に変更
-
-	// アフィン変換行列を再計算
-	worldTransform_.matWorld_ = MakeAffineMatrix(worldTransform_.scale_, worldTransform_.rotation_, worldTransform_.translation_);
-
-	// 定数バッファに行列を転送
-	
-	worldTransform_.TransferMatrix();
-}
-
-
-void Player::Draw() {
-	model_->Draw(worldTransform_, *camera_);
-	
-}
+void Player::Update() { worldTransform_.TransferMatrix(); }
+void Player::Draw() { model_->Draw(worldTransform_, *camera_); }
 
 Player::~Player() {
 	// リソースが不要ならばここで解放
